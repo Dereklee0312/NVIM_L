9b3b66f74eaa0c120162aa5b2aeffe0bfb76f67b
derek_lee <20820343@student.curtin.edu.au>
2022-12-28 01:30:28 +0400

    feat: Turnt statusline background into transparent

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -31,7 +31,9 @@ function M.config()
 		--- You can override specific color groups to use other groups or a hex color
 		--- function will be called with a ColorScheme table
 		---@param colors ColorScheme
-		on_colors = function(colors) end,
+		on_colors = function(colors)
+			colors.bg_statusline = "none"
+		end,
 
 		--- You can override specific highlights to use other groups or a hex color
 		--- function will be called with a Highlights and ColorScheme table
