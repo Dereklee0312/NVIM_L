75c7f53108c49e7451883c3e4840b8071865f694
derek_lee <20820343@student.curtin.edu.au>
2022-12-28 01:15:24 +0400

    chore: Cleanup

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,20 +1,9 @@
 local M = {
 	"lewis6991/gitsigns.nvim",
 	event = "VeryLazy",
-	-- lazy = false
 }
 
 function M.config()
-	-- local options = {
-	-- 	signs = {
-	-- 		add = { hl = "DiffAdd", text = "│", numhl = "GitSignsAddNr" },
-	-- 		change = { hl = "DiffChange", text = "│", numhl = "GitSignsChangeNr" },
-	-- 		delete = { hl = "DiffDelete", text = "", numhl = "GitSignsDeleteNr" },
-	-- 		topdelete = { hl = "DiffDelete", text = "‾", numhl = "GitSignsDeleteNr" },
-	-- 		changedelete = { hl = "DiffChangeDelete", text = "~", numhl = "GitSignsChangeNr" },
-	-- 	},
-	-- }
-	-- require("gitsigns").setup(options)
 	require("gitsigns").setup()
 end
 
