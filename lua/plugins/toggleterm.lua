91d6aeaca603b697d54dbb1f1804b82d7caf7803
derek_lee <20820343@student.curtin.edu.au>
2022-12-28 12:04:49 +0400

    feat: Added toggleterm plugin

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,50 @@
+local M = {
+	"akinsho/toggleterm.nvim",
+	event = "VeryLazy",
+}
+
+function M.config()
+	require("toggleterm").setup({
+		size = 100,
+		open_mapping = [[<A-i>]],
+		direction = "float",
+		float_opts = {
+			border = "curved",
+			winblend = 0,
+			highlights = {
+				border = "Normal",
+				background = "Normal",
+			},
+		},
+	})
+end
+
+function M.init()
+	local Terminal = require("toggleterm.terminal").Terminal
+	local lazygit = Terminal:new({ cmd = "lazygit" })
+
+	function _lazy_toggle()
+		lazygit:toggle()
+	end
+
+	function _G.set_terminal_keymaps()
+		local opts = { buffer = 0 }
+		local map = vim.keymap.set
+		map("t", "<C-n>", [[<C-\><C-n>]], opts)
+		map("t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+		map("t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+		map("t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+		map("t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+	end
+
+	local nmap = vim.api.nvim_set_keymap
+	nmap("", "<A-l>", "<cmd>lua _lazy_toggle()<CR>", { noremap = true, silent = true })
+	nmap("", "<A-h>", "<cmd>ToggleTerm size=20 direction=horizontal<CR>", { noremap = true, silent = true })
+	nmap("", "<A-i>", "<cmd>ToggleTerm size=100 direction=float<CR>", { noremap = true, silent = true })
+	nmap("", "<A-v>", "<cmd>ToggleTerm size=110 direction=vertical<CR>", { noremap = true, silent = true })
+
+	-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+	vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+end
+
+return M
