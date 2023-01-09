32d92e78ebd9afe4252272b3e8efe27458c87496
derek_lee <20820343@student.curtin.edu.au>
2023-01-10 00:01:56 +0400

    feat: Dismissed unnecessary notification from lsp

--- lua/core/autocmd.lua
+++ lua/core/autocmd.lua
@@ -6,41 +6,50 @@ local autocmd = vim.api.nvim_create_autocmd
 local opt = vim.opt
 
 autocmd({ "RecordingEnter", "RecordingLeave", "CmdlineEnter", "CmdlineLeave" }, {
-  callback = function()
-    if vim.g.CMDHEIGHT == 0 then
-      opt.cmdheight = 1
-      vim.g.CMDHEIGHT = 1
-    else
-      opt.cmdheight = 0
-      vim.g.CMDHEIGHT = 0
-    end
-  end,
+	callback = function()
+		if vim.g.CMDHEIGHT == 0 then
+			opt.cmdheight = 1
+			vim.g.CMDHEIGHT = 1
+		else
+			opt.cmdheight = 0
+			vim.g.CMDHEIGHT = 0
+		end
+	end,
 })
 
 ---Highlight yanked text
 au("TextYankPost", {
-  group = ag("yank_highlight", {}),
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank { higroup = "IncSearch", timeout = 300 }
-  end,
+	group = ag("yank_highlight", {}),
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 300 })
+	end,
 })
 
 -- Automatically deletes all trailing whitespace and newlines at end of file on save. & reset cursor position
 vim.api.nvim_exec(
-  [[
+	[[
 autocmd BufWritePre * let currPos = getpos(".")
 autocmd BufWritePre * %s/\s\+$//e
 autocmd BufWritePre * %s/\n\+\%$//e
 autocmd BufWritePre *.[ch] %s/\%$/\r/e
 autocmd BufWritePre * cal cursor(currPos[1], currPos[2])
 ]],
-  false
+	false
 )
 
 -- Blinking Cursor
-vim.cmd [[
+vim.cmd([[
 		:set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50
 		  \,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor
 		  \,sm:block-blinkwait175-blinkoff150-blinkon175
-]]
+]])
+
+local notify = vim.notify
+vim.notify = function(msg, ...)
+	if msg:match("warning: multiple different client offset_encodings") then
+		return
+	end
+
+	notify(msg, ...)
+end
